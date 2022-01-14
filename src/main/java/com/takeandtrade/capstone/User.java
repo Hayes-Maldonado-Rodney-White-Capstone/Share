6c3755d5cc6c7812c4e887a8e1f6f5bcea2c7b5d
MacOS <josemmaldonado50@gmail.com>
2022-01-14 09:59:20 -0600

    user table

---
+++ src/main/java/com/takeandtrade/capstone/User.java
@@ -0,0 +1,49 @@
+package com.takeandtrade.capstone;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 255)
+    private String first_name;
+
+    @Column(nullable = false, length = 255)
+    private String last_name;
+
+    @Column (nullable = false, length = 255)
+    private String username;
+
+    @Column (nullable = false)
+    private long date_of_birth;
+
+    @Column (nullable = false, length = 255)
+    private String email;
+
+    @Column (nullable = false, length = 255)
+    private String password;
+
+    @Column (nullable = false)
+    private int phone_number;
+
+    @Column (nullable = false, length = 255)
+    private String city;
+
+    @Column (nullable = false, length = 2)
+    private String state;
+
+    @Column (nullable = false)
+    private int zip_code;
+
+//    @ManyToOne
+//    @JoinColumn (name = "role_id")
+//    private User role;
+
+
+
+
+}
