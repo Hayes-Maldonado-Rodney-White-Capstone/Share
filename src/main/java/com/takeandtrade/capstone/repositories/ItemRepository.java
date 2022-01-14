6eba4eeac0ca92c38b6714e4d38445f2e63bf8c3
Mary White <mary.a.white28@gmail.com>
2022-01-14 12:56:21 -0600

    added the ItemRepository in the repositories directory

---
+++ src/main/java/com/takeandtrade/capstone/repositories/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.takeandtrade.capstone.repositories;
+
+import com.takeandtrade.capstone.models.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
