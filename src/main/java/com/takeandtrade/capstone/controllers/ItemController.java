1411d365ecc30425633887f22ed0a35fc284c4e0
Mary White <mary.a.white28@gmail.com>
2022-01-16 18:36:05 -0600

    working on the items/create form so users can create a new post, and working on items index

--- src/main/java/com/takeandtrade/capstone/controllers/ItemController.java
+++ src/main/java/com/takeandtrade/capstone/controllers/ItemController.java
@@ -6,6 +6,7 @@ import com.takeandtrade.capstone.models.Item;
 import com.takeandtrade.capstone.models.User;
 import com.takeandtrade.capstone.repositories.CategoryRepository;
 import com.takeandtrade.capstone.repositories.ItemRepository;
+import com.takeandtrade.capstone.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,10 +24,12 @@ public class ItemController {
     //dependency injection
     private final ItemRepository itemDao;
     private final CategoryRepository categoryDao;
+    private final UserRepository userDao;
 
-    public ItemController(ItemRepository itemDao, CategoryRepository categoryDao){
+    public ItemController(ItemRepository itemDao, CategoryRepository categoryDao, UserRepository userDao){
         this.itemDao = itemDao;
         this.categoryDao = categoryDao;
+        this.userDao = userDao;
     }
 
     @GetMapping("/items/create")
@@ -39,12 +43,17 @@ public class ItemController {
 
     @PostMapping("/items/create")
     public String addNewItem(@ModelAttribute Item item, Model model) {
-        User itemLender = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();  //this grabs the logged in user
-        item.setUser(itemLender);
-        model.addAttribute("timestamp", Instant.now()); //how do I get the timestamp generated?
+//        User itemLender = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();  //this grabs the logged in user
+//        item.setUser(itemLender);
 
+        //we'll use this code below for testing so we don't have to log in each time, when the create form is function use the code above for production
+        User user = userDao.getById(1L);
+        item.setUser(user);
+        //
+        model.addAttribute("zipCode", user.getZipCode()); //adding this to see if it will pull in user zip code
+        item.setDatePosted(LocalDateTime.now());
         itemDao.save(item);
-        return "redirect:items/itemsindex";
+        return "redirect:/items";
     }
 
     @GetMapping("/items")
