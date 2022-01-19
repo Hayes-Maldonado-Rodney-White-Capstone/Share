a213bd3f5af1bf4b7b7d55b43c8f999438813efc
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2022-01-18 20:04:46 -0600

    Made Message Index show users

--- src/main/java/com/takeandtrade/capstone/controllers/MessageController.java
+++ src/main/java/com/takeandtrade/capstone/controllers/MessageController.java
@@ -1,5 +1,6 @@
 package com.takeandtrade.capstone.controllers;
 
+import com.takeandtrade.capstone.models.Item;
 import com.takeandtrade.capstone.models.User;
 import com.takeandtrade.capstone.repositories.CategoryRepository;
 import com.takeandtrade.capstone.repositories.ItemRepository;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MessageController {
@@ -26,9 +28,24 @@ public class MessageController {
     @GetMapping("/messages/messagesindex")
     public String viewMessageIndex(Model model) {
 
-        model.addAttribute("messages", messageDao.findAll());
+        model.addAttribute("users", userDao.findAll());
 
         return "messages/messagesindex";
     }
+
+    @GetMapping("/messages/{otherUserId}")
+    public String pickAReciever(Model model, @PathVariable long recieverId) {
+
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userDao.getById(loggedInUser.getId());
+        model.addAttribute("loggedinuser", user);
+
+        User reciever = userDao.getById(recieverId);
+        model.addAttribute("reciever", reciever);
+
+        return "/messages/viewmessage";
+    }
+
+
 
 }
