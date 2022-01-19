972002cd9c28ed0e42babbc9c314166f33a80a8d
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2022-01-18 19:40:08 -0600

    Linked up MessagesIndex to MessageController

--- src/main/java/com/takeandtrade/capstone/controllers/MessageController.java
+++ src/main/java/com/takeandtrade/capstone/controllers/MessageController.java
@@ -1,5 +1,34 @@
 package com.takeandtrade.capstone.controllers;
 
+import com.takeandtrade.capstone.models.User;
+import com.takeandtrade.capstone.repositories.CategoryRepository;
+import com.takeandtrade.capstone.repositories.ItemRepository;
+import com.takeandtrade.capstone.repositories.MessageRepository;
+import com.takeandtrade.capstone.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
 public class MessageController {
+
+    private final MessageRepository messageDao;
+    private final UserRepository userDao;
+
+    //dependency injection
+    public MessageController(MessageRepository messageDao, UserRepository userDao) {
+        this.messageDao = messageDao;
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/messages/messagesindex")
+    public String viewMessageIndex(Model model) {
+
+        model.addAttribute("messages", messageDao.findAll());
+
+        return "messages/messagesindex";
+    }
 
 }
