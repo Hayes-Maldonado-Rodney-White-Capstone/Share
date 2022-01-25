0a54d920ecfdf406edef2908647d47109484c2c7
MacOS <josemmaldonado50@gmail.com>
2022-01-25 14:37:50 -0600

    user can view average rating

--- src/main/java/com/takeandtrade/capstone/repositories/ReviewRepository.java
+++ src/main/java/com/takeandtrade/capstone/repositories/ReviewRepository.java
@@ -2,9 +2,14 @@ package com.takeandtrade.capstone.repositories;
 
 import com.takeandtrade.capstone.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Query("SELECT AVG(rating) FROM Review WHERE producer.id = ?1")
+    public Double findRatingAverage(Long producerId);
+
 
 }
